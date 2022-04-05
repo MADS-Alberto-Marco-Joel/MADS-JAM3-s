bd85ec42113d363f211080a3410bece92e6726f2
MarcoAraujo16 <marcoaraujo1616@gmail.com>
2022-04-05 15:00:33 +0100

    UpdateSprint1
    Base de dados criada,testes implementados e tabela especies criada

--- ProjetoArvore/ProjetoArvore/RepositorioArvores.cs
+++ ProjetoArvore/ProjetoArvore/RepositorioArvores.cs
@@ -15,19 +15,62 @@ namespace ProjetoArvore
         {
             list_arvore = new List<Arvore>();
             //Inserir um valor à lista para testar inserçoes repetidas
-            Arvore arvore = new() { Id = 1, NomeCientifico = "Arvoris Grandolis", Altura = 2.50f, Diametro = 3.60f, Perimetro = 100f, Classificacao = 10, IdConstrucao = 1, Localizacao = "porto", ValidFrom = DateOnly.Parse("2022-02-06"), ValidTo = DateOnly.Parse("2022-02-06") };
+            Arvore arvore = new() { Id = 1, NomeCientifico = "Arvoris Grandolis", Altura = 2.50f, Diametro = 3.60f, Perimetro = 100f, Classificacao = 10, IdConstrucao = 1, Localizacao = "porto", ValidFrom = DateOnly.Parse("2022-02-06"), ValidTo = default };
             list_arvore.Add(arvore);
         }
         public bool AddArvore(Arvore a)
         {
-            bool existe =list_arvore.Exists(x => x.Id == a.Id);
-            if (existe == false)
+            try
             {
-                list_arvore.Add(a);
-                return false;
+                bool existe = list_arvore.Exists(x => x.Id == a.Id);
+                if (existe == false)
+                {
+                    list_arvore.Add(a);
+                    return false;
+                }
+                else
+                    return true;
             }
-            else
-            return true;
+            catch
+            {
+                return true;
+            }
         }
+
+        public bool EditArvore(Arvore a)
+        {
+            int index = list_arvore.FindIndex(x => x.Id == a.Id);
+            if (index == -1)
+                return false;
+            else
+            {
+                list_arvore[index] = a;
+                return true;
+            }
+           // MessageBox.Show(index.ToString());
+
+        }
+
+        public bool MorteArvore(int id,DateOnly dt)
+        {
+            int index = list_arvore.FindIndex(x => x.Id == id);
+            if (index == -1)
+                return false;
+            else
+            {
+                DateOnly validFrom = list_arvore[index].ValidFrom;
+                if (dt > validFrom)
+                {
+                    list_arvore[index].ValidTo = dt;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            
+        }
+
     }
 }
--- ProjetoArvore/TesteProjetoArvore/TestRegisto.cs
+++ ProjetoArvore/TesteProjetoArvore/TestRegisto.cs
@@ -11,9 +11,11 @@ namespace TesteProjetoArvore
         {
             //Assert.AreEqual(false,"1","Arvore Plantis",3.50f,);
             RepositorioArvores x = new RepositorioArvores();
-            Arvore arvore= new() { Id = 2, NomeCientifico = "Arvoris Grandolis", Altura = 2.50f, Diametro = 3.60f, Perimetro = 100f, Classificacao = 10, IdConstrucao = 1, Localizacao = "porto", ValidFrom = DateOnly.Parse("2022-02-06"), ValidTo = DateOnly.Parse("2022-02-06") };
+            Arvore arvore= new() { Id = 2, NomeCientifico = "Arvoris Grandolis", Altura = 2.50f, Diametro = 3.60f, Perimetro = 100f, Classificacao = 10, IdConstrucao = 1, Localizacao = "porto", ValidFrom = DateOnly.Parse("2022-02-06"), ValidTo = default };
             //Arvore arvore2 = new() { Id = 1, NomeCientifico = "Arvoris Grandolis", Altura = 2.50f, Diametro = 3.60f, Perimetro = 100f, Classificacao = 10, IdConstrucao = 1, Localizacao = "porto", ValidFrom = DateOnly.Parse("2022-02-06"), ValidTo = DateOnly.Parse("2022-02-06") };
             Assert.AreEqual(false, x.AddArvore(arvore));
+            Assert.AreEqual(true, x.EditArvore(arvore));
+            Assert.AreEqual(true, x.MorteArvore(1, DateOnly.Parse("2022-05-04")));
 
 
         }
