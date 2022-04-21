eb6355e47febb43024ac24198dd17840f0369ea1
MarcoAraujo16 <marcoaraujo1616@gmail.com>
2022-04-21 19:08:18 +0100

    ver1.3

---
+++ ProjetoArvore/ProjetoArvore/Observacoes.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ProjetoArvore
+{
+    partial class Observacoes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabTronco = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabTronco.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabTronco
+            // 
+            this.tabTronco.Controls.Add(this.tabPage1);
+            this.tabTronco.Controls.Add(this.tabPage2);
+            this.tabTronco.Location = new System.Drawing.Point(26, 49);
+            this.tabTronco.Name = "tabTronco";
+            this.tabTronco.SelectedIndex = 0;
+            this.tabTronco.Size = new System.Drawing.Size(751, 374);
+            this.tabTronco.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(743, 346);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 72);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Observacoes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabTronco);
+            this.Name = "Observacoes";
+            this.Text = "Observacoes";
+            this.tabTronco.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TabControl tabTronco;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+    }
+}
--- ProjetoArvore/TesteProjetoArvore/TestRegisto.cs
+++ ProjetoArvore/TesteProjetoArvore/TestRegisto.cs
@@ -7,7 +7,7 @@ namespace TesteProjetoArvore
     public class Tests
     {
         RepositorioArvores x = new RepositorioArvores();
-        Arvore arvore = new() { Id = 2, NomeCientifico = "Arvoris Grandolis", Altura = 2.50f, Diametro = 3.60f, Perimetro = 100f, Classificacao = 10, IdConstrucao = 1, Localizacao = "porto", ValidFrom = DateOnly.Parse("2022-02-06"), ValidTo = default };
+        Arvore arvore = new() { Id = 2, NomeCientifico = 1, Altura = 2.50f, Diametro = 3.60f, Perimetro = 100f, Classificacao = 10, IdConstrucao = 1, Localizacao = "porto", ValidFrom = DateOnly.Parse("2022-02-06"), ValidTo = default };
 
         [Test]
         public void ValidarRegisto()
